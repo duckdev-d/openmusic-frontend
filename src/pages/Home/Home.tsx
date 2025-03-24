c6ccf612a88a454ab80a6f4ab26bc03793f5168e
Dmitry <dmitry.workinbox@gmail.com>
2025-03-24 11:36:15 -0600

    add basic home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,8 @@ export default function HomePage() {
   return (
     <>
     <h1>home page</h1>
-    <p>your access token is {localStorage.getItem('jwt')}</p>
+    <br />
+    <p style={{color: 'violet'}}>your access token is: {localStorage.getItem('jwt')}</p>
     </>
   )
 }
