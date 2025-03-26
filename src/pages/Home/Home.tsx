b0774c351b36340db97816edabf2abfff90c21e2
Dmitry <dmitry.workinbox@gmail.com>
2025-03-26 14:04:26 -0600

    add files for pages

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
-
+import "./Home.css";
 
 export default function HomePage() {
   return (
-    <>
-    <h1>home page</h1>
-    <br />
-    <p style={{color: 'violet'}}>your access token is: {localStorage.getItem('jwt')}</p>
-    </>
-  )
+    <div className="home">
+      <h1>home page</h1>
+      <br />
+      <p style={{ color: "violet" }}>
+        your access token is: {localStorage.getItem("jwt")}
+      </p>
+    </div>
+  );
 }
