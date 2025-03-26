163ddafcfec92ea8b243dc7fb5789828688930ae
Dmitry <dmitry.workinbox@gmail.com>
2025-03-26 14:04:02 -0600

    add left menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-import { Routes, Route } from 'react-router-dom'
-import LoginPage from './pages/LogIn/LogIn'
-import SignUpPage from './pages/SingUp/SingUp'
-import HomePage from './pages/Home/Home'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LogIn/LogIn";
+import SignUpPage from "./pages/SingUp/SingUp";
+import HomePage from "./pages/Home/Home";
+import SearchPage from "./pages/Search/Search";
+import FavouritesPage from "./pages/Favourites/Favourites";
+import PlaylistsPage from "./pages/Playlists/Playlists";
+import Menu from "./components/Menu/Menu";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      <Routes>
-        <Route path='/login' element={<LoginPage />}/>
-        <Route path='/signup' element={<SignUpPage />}/>
-        <Route path='/home' element={<HomePage />}/>
-      </Routes>
-    </>
-  )
+    <div className="app">
+      <Menu
+        iconWidth="2vw"
+        height="95vh"
+        width="4vw"
+        borderRadius="1vw"
+        gap="1.5vw"
+      />
+      <div className="current-page">
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/favourites" element={<FavouritesPage />} />
+          <Route path="/playlists" element={<PlaylistsPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,66 @@
+import { NavLink } from "react-router-dom";
+import homeActive from "../../assets/home-primary.svg";
+import homeDisabled from "../../assets/home-secondary.svg";
+import searchActive from "../../assets/search-primary.svg";
+import searchDisabled from "../../assets/search-secondary.svg";
+import favouritesActive from "../../assets/favourites-primary.svg";
+import favouritesDisabled from "../../assets/favourites-secondary.svg";
+import playlistsActive from "../../assets/playlists-primary.svg";
+import playlistsDisabled from "../../assets/playlists-secondary.svg";
+import "./Menu.css";
+
+interface Props {
+  iconWidth: string;
+  width: string;
+  height: string;
+  borderRadius: string;
+  gap: string;
+}
+
+export default function Menu({
+  iconWidth,
+  width,
+  height,
+  borderRadius,
+  gap,
+}: Props) {
+  const menuItems = [
+    { path: "/home", activeIcon: homeActive, disabledIcon: homeDisabled },
+    { path: "/search", activeIcon: searchActive, disabledIcon: searchDisabled },
+    {
+      path: "/favourites",
+      activeIcon: favouritesActive,
+      disabledIcon: favouritesDisabled,
+    },
+    {
+      path: "/playlists",
+      activeIcon: playlistsActive,
+      disabledIcon: playlistsDisabled,
+    },
+  ];
+
+  return (
+    <div
+      className="menu"
+      style={{
+        width: width,
+        height: height,
+        borderRadius: borderRadius,
+        gap: gap,
+      }}
+    >
+      {menuItems.map(({ path, activeIcon, disabledIcon }) => (
+        <NavLink key={path} to={path}>
+          {({ isActive }) => (
+            <img
+              src={isActive ? activeIcon : disabledIcon}
+              style={{ width: iconWidth }}
+              alt=""
+              draggable="false"
+            />
+          )}
+        </NavLink>
+      ))}
+    </div>
+  );
+}
