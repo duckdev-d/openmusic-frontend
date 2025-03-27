729fd495fd876216e19faa8a125b6c027590a9b5
Dmitry <dmitry.workinbox@gmail.com>
2025-03-27 10:37:10 -0600

    add song component

---
+++ src/components/Song/Song.tsx
@@ -0,0 +1,75 @@
+import LikeButton from "../LikeButton/LikeButton";
+import PrimaryText from "../PrimaryText/PrimaryText";
+import SecondaryText from "../SecondaryText/SecondaryText";
+import "./Song.css";
+
+interface Props {
+  width?: string;
+  coverWidth?: string;
+  title?: string;
+  artist?: string;
+  duration?: string;
+  padding?: string;
+}
+
+export default function Song({
+  width = "10vw",
+  coverWidth = "2vw",
+  title = "title",
+  artist = "artist",
+  duration = "2:25",
+  padding = "1vw",
+}: Props) {
+  return (
+    <div
+      className="song"
+      style={{
+        width: width,
+        padding: padding,
+        borderRadius: "0.5vw",
+        alignItems: "center",
+        position: "relative",
+      }}
+    >
+      <div
+        className="cover"
+        style={{
+          width: coverWidth,
+          height: coverWidth,
+          background: "rgb(122, 122, 122)",
+          borderRadius: "0.5vw",
+        }}
+      />
+
+      <div
+        className="text-wrap"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          marginLeft: "2%",
+          justifyContent: "center",
+          gap: "10%",
+        }}
+      >
+        <PrimaryText size="1.2vw">{title}</PrimaryText>
+        <SecondaryText size="1vw">{artist}</SecondaryText>
+      </div>
+
+      <div
+        className="like-duration-wrap"
+        style={{
+          position: "absolute",
+          right: "1vw",
+          display: "flex",
+          alignItems: "center",
+          gap: "2vw",
+        }}
+      >
+        <SecondaryText className="song-duration" size="1vw">
+          {duration}
+        </SecondaryText>
+        <LikeButton width="25vw" />
+      </div>
+    </div>
+  );
+}
