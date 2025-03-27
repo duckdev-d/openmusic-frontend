17be2009b3c4a9f36277b21d318bf4b894482cbe
Dmitry <dmitry.workinbox@gmail.com>
2025-03-27 10:35:17 -0600

    add like button component

---
+++ src/components/LikeButton/LikeButton.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import heartOn from "../../assets/favourites-primary.svg";
+import heartOff from "../../assets/favourites-secondary.svg";
+
+interface Props {
+  width: string;
+}
+
+export default function LikeButton({ width }: Props) {
+  const [image, setImage] = useState(heartOff);
+
+  function handleClick(e: React.MouseEvent) {
+    if (image === heartOff) {
+      setImage(heartOn);
+    } else {
+      setImage(heartOff);
+    }
+  }
+
+  return (
+    <div onClick={handleClick} style={{ cursor: "pointer" }}>
+      <img width={width} src={image} alt="" />
+    </div>
+  );
+}
