81d7414eede5f66bb4e3a1f7977309adca1b6a4c
Dmitry <dmitry.workinbox@gmail.com>
2025-03-28 02:26:05 -0600

    add border radius property to line component

--- src/components/Line/Line.tsx
+++ src/components/Line/Line.tsx
@@ -1,18 +1,28 @@
 interface Props {
-    width: string,
-    height: string,
-    color: string,
-    mBottom?: string,
+  width: string;
+  height: string;
+  color?: string;
+  mBottom?: string;
+  borderRadius?: string;
 }
 
-export default function Line({width, height,  color, mBottom='0'}: Props) {
+export default function Line({
+  width,
+  height,
+  color = "white",
+  mBottom = "0",
+  borderRadius = "0",
+}: Props) {
   return (
-    <div 
-        style={{width: width, 
-                height: height, 
-                background: color,
-                minHeight: '3px',
-                marginBottom: mBottom}}>
-    </div>
-  )
+    <div
+      style={{
+        width: width,
+        height: height,
+        background: color,
+        minHeight: "3px",
+        marginBottom: mBottom,
+        borderRadius: borderRadius,
+      }}
+    ></div>
+  );
 }
