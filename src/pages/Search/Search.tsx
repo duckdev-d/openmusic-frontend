5ade4ec16f94889488604333b1e7ffbdb57260db
Dmitry <dmitry.workinbox@gmail.com>
2025-03-28 01:12:40 -0600

    add search page basic layout

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import Search from "../../components/Search/Search";
 
 export default function SearchPage() {
-  return <div>Search</div>;
+  return (
+    <div
+      className="search"
+      style={{ display: "flex", justifyContent: "center" }}
+    >
+      <Search width="40vw" />
+    </div>
+  );
 }
