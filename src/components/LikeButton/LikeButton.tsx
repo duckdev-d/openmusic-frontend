e8e61b311f42f4a6b549929ae3b7bd746089ee69
Dmitry <dmitry.workinbox@gmail.com>
2025-03-28 02:14:47 -0600

    add visibility property to like button

--- src/components/LikeButton/LikeButton.tsx
+++ src/components/LikeButton/LikeButton.tsx
@@ -4,9 +4,10 @@ import heartOff from "../../assets/favourites-secondary.svg";
 
 interface Props {
   width: string;
+  visibility: "visible" | "hidden";
 }
 
-export default function LikeButton({ width }: Props) {
+export default function LikeButton({ width, visibility }: Props) {
   const [image, setImage] = useState(heartOff);
 
   function handleClick(e: React.MouseEvent) {
@@ -18,7 +19,10 @@ export default function LikeButton({ width }: Props) {
   }
 
   return (
-    <div onClick={handleClick} style={{ cursor: "pointer" }}>
+    <div
+      onClick={handleClick}
+      style={{ cursor: "pointer", visibility: visibility }}
+    >
       <img draggable="false" width={width} src={image} alt="" />
     </div>
   );
