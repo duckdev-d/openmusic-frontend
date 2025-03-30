f75faf825b48f9e947fd927d1a1fcb837e015478
Dmitry <dmitry.workinbox@gmail.com>
2025-03-30 04:46:33 -0600

    make auth functions asyncronous

--- src/services/auth.tsx
+++ src/services/auth.tsx
@@ -1,45 +1,43 @@
-import axios from "axios"
+import axios from "axios";
 
+export async function logIn(username: string, password: string) {
+  const API_URL = import.meta.env.VITE_API_URL;
+  const url = `${API_URL}/token`;
 
-export function logIn(username: string, password: string) {
+  const formData = new URLSearchParams();
+  formData.append("username", username);
+  formData.append("password", password);
 
-  const API_URL = import.meta.env.VITE_API_URL
-  const url = `${API_URL}/token`
-
-  const formData = new URLSearchParams()
-  formData.append('username', username)
-  formData.append('password',password)
-
-  axios.post<{access_token: string}>(url, formData, {
+  const response = await axios.post<{ access_token: string }>(url, formData, {
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
+      "Content-Type": "application/x-www-form-urlencoded",
     },
-  })
-  
-  .then(response => {
-    const jwt = response.data.access_token
-    localStorage.setItem('jwt', jwt)
+  });
+
+  if (!(response.status === 201)) {
+    throw new Error();
   }
-  ) 
+
+  const jwt = response.data.access_token;
+  localStorage.setItem("jwt", jwt);
 }
 
-
-export function signUp(username: string, password: string) {
-
-  const API_URL = import.meta.env.VITE_API_URL
-  const url = `${API_URL}/users`
+export async function signUp(username: string, password: string) {
+  const API_URL = import.meta.env.VITE_API_URL;
+  const url = `${API_URL}/users`;
 
   const formData = {
     username: username,
     password: password,
-  }
+  };
 
-  axios.post<{access_token: string}>(url, formData, {
+  const response = await axios.post<{ access_token: string }>(url, formData, {
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/x-www-form-urlencoded",
     },
-  })
-  .catch(error => {
-    console.log('could not create account')
   });
+
+  if (!(response.status === 201)) {
+    throw new Error();
+  }
 }
