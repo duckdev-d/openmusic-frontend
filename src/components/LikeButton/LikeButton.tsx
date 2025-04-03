3a4e1cc148f2eb98de93ae55b67df207e9718f1b
Dmitry <dmitry.workinbox@gmail.com>
2025-04-03 12:45:46 -0600

    accept songId and enabled as parameters, refactor handleClick function

--- src/components/LikeButton/LikeButton.tsx
+++ src/components/LikeButton/LikeButton.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import heartOn from "../../assets/heart-white.svg";
 import heartOff from "../../assets/favourites-secondary.svg";
+import {
+  addSongToFavourites,
+  removeSongFromFavourites,
+} from "../../services/api";
 
 interface Props {
   width: string;
+  songId: number;
+  enabled: boolean;
   visibility?: "visible" | "hidden";
   onClick?: () => void;
 }
@@ -11,20 +17,27 @@ interface Props {
 export default function LikeButton({
   width,
   visibility = "visible",
-  onClick,
+  songId,
+  enabled = false,
 }: Props) {
-  const [image, setImage] = useState(heartOff);
+  const [image, setImage] = useState(enabled ? heartOn : heartOff);
 
-  function handleClick(e: React.MouseEvent) {
-    if (onClick) {
-      onClick();
-    }
-
+  function changeImage() {
     if (image === heartOff) {
       setImage(heartOn);
     } else {
       setImage(heartOff);
     }
+  }
+
+  function handleClick(e: React.MouseEvent) {
+    if (image === heartOff) {
+      addSongToFavourites(songId);
+    } else {
+      removeSongFromFavourites(songId);
+    }
+
+    changeImage();
   }
 
   return (
