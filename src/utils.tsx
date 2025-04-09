4a9f06ee654def0e89af2461223bf526365f6d27
Dmitry <dmitry.workinbox@gmail.com>
2025-04-09 10:43:04 -0600

    add function for song titles formatting

--- src/utils.tsx
+++ src/utils.tsx
@@ -3,3 +3,11 @@ export function formatDuration(seconds: number) {
   const remainingSeconds = seconds % 60;
   return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
 }
+
+export function formatSongTitle(title: string) {
+  if (title.length > 12) {
+    return title.substring(0, 12) + "...";
+  } else {
+    return title;
+  }
+}
